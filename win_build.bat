a0bf9dfe7dcf08217c3c0ef7d390c3998a42c8c0
wuzhaohui <thwuzhaohui@163.com>
2016-04-24 01:17:18 +0800

    add win bat

---
+++ win_build.bat
@@ -0,0 +1,10 @@
+set "version=2.0.0"
+del builds\zotero-scholar-citations-%version%-fx.xpi
+rd /s/q builds\temp
+mkdir builds\temp
+mkdir builds\temp\chrome
+xcopy /e chrome builds\temp\chrome
+copy chrome.manifest builds\temp
+copy install.rdf builds\temp
+Wzzip -r -p builds\zotero-scholar-citations-%version%-fx.xpi builds\temp
+rd /s/q builds\temp
